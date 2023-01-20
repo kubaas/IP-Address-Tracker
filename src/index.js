d4ede119ba0a8e3c648bffe32adc5c1992d7069e
Jakub Smus <kuba.smus@gmail.com>
2023-01-20 22:35:10 +0100

    Add linter. webpack

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import iconMarker from 'leaflet/dist/images/marker-icon.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+var L = window.L;
+
+const API_KEY = 'at_X9jH5DZGpX7HsGe7WV5PAlsVTNcov';
+const GEOLOCATION_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}`;
+const GEOLOCATION_WITH_IP_URL = `${GEOLOCATION_URL}&ipAddress=`;
+
+const IP_REGEXP = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/;
+
+const button = document.querySelector('.search-panel__button');
+const form = document.querySelector('.search-panel__form');
+const ipAddress = document.getElementById('ip-address');
+const region = document.getElementById('location');
+const timezone = document.getElementById('timezone');
+const isp = document.getElementById('isp');
+
+const map = L.map('map');
+const marker = L.marker([0, 0]).addTo(map);
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: iconMarker,
+  shadowUrl: iconShadow,
+});
+
+/** Event listeners */
+button.addEventListener('click', event => onSearchLocation(event));
+form.addEventListener('submit', event => onSearchLocation(event));
+
+/** Main logic */
+getLocation().then((result) => showIpAddress(result));
+getCurrentPosition();
+
+function showIpAddress(result) {
+  ipAddress.innerHTML = result.ip;
+  region.innerHTML = result.location.city + ', ' + result.location.country + ' ' + result.location.postalCode;
+  timezone.innerHTML = 'UTC ' + result.location.timezone;
+  isp.innerHTML = result.isp;
+}
+
+function getCurrentPosition() {
+  if (!navigator.geolocation) return;
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    setMapNewLocation(latitude, longitude, true);
+  });
+}
+
+function onSearchLocation(event) {
+  event.preventDefault();
+  const ip = document.querySelector('.search-panel__input').value;
+
+  if (!ip) return;
+  if(!IP_REGEXP.test(ip)) { alert('Invalid IP address!'); return; }
+
+  getLocation(ip).then((result) => {
+    showIpAddress(result);
+    setMapNewLocation(result.location.lat, result.location.lng);
+  });
+}
+
+/** Geolocation API results */
+function getLocation(ipAddress) {
+  return fetch(ipAddress ? (GEOLOCATION_WITH_IP_URL + ipAddress) : GEOLOCATION_URL, { method: 'GET' })
+    .then((result) => result.json());
+}
+
+/** Leafletjs map */
+function setMapNewLocation(latitude, longitude, shouldShowPopup) {
+  const center = [latitude, longitude];
+  map.setView(center, 14);
+  marker.setLatLng(center);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  showOrHidePopup(shouldShowPopup);
+}
+
+function showOrHidePopup(shouldShowPopup) {
+  shouldShowPopup ? marker.bindPopup('<b>Here you are!</b>').openPopup() : map.closePopup();
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: ['url-loader']
+      }
+    ]
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+  },
+};
