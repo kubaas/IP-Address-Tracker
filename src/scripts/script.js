405e7dd2609d18a4c0875fb00a790a72eb118b92
Jakub Smus <kuba.smus@gmail.com>
2023-01-20 16:24:04 +0100

    Cleanup, add linter

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -23,52 +23,52 @@ getLocation().then((result) => showIpAddress(result));
 getCurrentPosition();
 
 function showIpAddress(result) {
-  ipAddress.innerHTML = result.ip;
-  region.innerHTML = result.location.city + ', ' + result.location.country + ' ' + result.location.postalCode;
-  timezone.innerHTML = 'UTC ' + result.location.timezone;
-  isp.innerHTML = result.isp;
+    ipAddress.innerHTML = result.ip;
+    region.innerHTML = result.location.city + ', ' + result.location.country + ' ' + result.location.postalCode;
+    timezone.innerHTML = 'UTC ' + result.location.timezone;
+    isp.innerHTML = result.isp;
 }
 
 function getCurrentPosition() {
-  if (!navigator.geolocation) return;
+    if (!navigator.geolocation) return;
 
-  navigator.geolocation.getCurrentPosition(position => {
-    const { latitude, longitude } = position.coords;
-    setMapNewLocation(latitude, longitude, true);
-  });
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setMapNewLocation(latitude, longitude, true);
+    });
 }
 
 function onSearchLocation(event) {
-  event.preventDefault();
-  const ip = document.querySelector('.search-panel__input').value;
+    event.preventDefault();
+    const ip = document.querySelector('.search-panel__input').value;
 
-  if (!ip) return;
-  if(!IP_REGEXP.test(ip)) { alert('Invalid IP address!'); return; }
+    if (!ip) return;
+    if(!IP_REGEXP.test(ip)) { alert('Invalid IP address!'); return; }
 
-  getLocation(ip).then((result) => {
-    showIpAddress(result);
-    setMapNewLocation(result.location.lat, result.location.lng);
-  });
+    getLocation(ip).then((result) => {
+        showIpAddress(result);
+        setMapNewLocation(result.location.lat, result.location.lng);
+    });
 }
 
 /** Geolocation API results */
 function getLocation(ipAddress) {
-  return fetch(ipAddress ? (GEOLOCATION_WITH_IP_URL + ipAddress) : GEOLOCATION_URL, { method: 'GET' })
-    .then((result) => result.json());
+    return fetch(ipAddress ? (GEOLOCATION_WITH_IP_URL + ipAddress) : GEOLOCATION_URL, { method: 'GET' })
+        .then((result) => result.json());
 }
 
 /** Leafletjs map */
 function setMapNewLocation(latitude, longitude, shouldShowPopup) {
-  const center = [latitude, longitude];
-  map.setView(center, 14);
-  marker.setLatLng(center);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
-  showOrHidePopup(shouldShowPopup)
+    const center = [latitude, longitude];
+    map.setView(center, 14);
+    marker.setLatLng(center);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    showOrHidePopup(shouldShowPopup);
 }
 
 function showOrHidePopup(shouldShowPopup) {
-  shouldShowPopup ? marker.bindPopup(`<b>Here you are!</b>`).openPopup() : map.closePopup();
+    shouldShowPopup ? marker.bindPopup('<b>Here you are!</b>').openPopup() : map.closePopup();
 }
